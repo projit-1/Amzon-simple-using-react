6f897debede900ffb174f28a22d3480ea696bc41
projit-1 <projitmondol@gmail.com>
2022-03-22 03:56:26 +0600

    git is connected

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(reponse => reponse.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart} ></Product>)
+                }
+
+            </div>
+            <div className="cart-conatiner">
+                <h4>Order Sumary</h4>
+                <p>Selected Item:{cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
